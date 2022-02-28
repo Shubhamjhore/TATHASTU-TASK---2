9844868c1015188bab9afa80062df7cb1a618f7e
Shubhamjhore <40377127+Shubhamjhore@users.noreply.github.com>
2022-02-28 14:09:55 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+public class Program1 {
+   public static void main(String[] args) {
+      double num1;
+      double num2;
+      double ans;
+      char op;
+      Scanner reader = new Scanner(System.in);
+      System.out.print("Enter two numbers: ");
+      num1 = reader.nextDouble();
+      num2 = reader.nextDouble();
+      System.out.print("\n Enter an operator (+, -, *, /): ");
+      op = reader.next().charAt(0);
+      switch(op) {
+         case '+': ans = num1 + num2;
+            break;
+         case '-': ans = num1 - num2;
+            break;
+         case '*': ans = num1 * num2;
+            break;
+         case '/': ans = num1 / num2;
+            break;
+      default: System.out.printf("Please enter correct operator");
+         return;
+      }
+      System.out.print("\n The calculation is :\n");
+      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+   }
+}
